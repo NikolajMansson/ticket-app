8dabda297532a9486b5a0489ddcedad38980ee4a
NikolajManssonEsSolutions <nikolaj.mansson@essolutions.se>
2022-01-10 15:06:08 +0100

    Added routing for comment view and edit view

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Ticket from './pages/Ticket';
 import Addticket from './pages/Addticket';
+import Editticket from './pages/Editticket';
+import Viewcomment from './pages/Viewcomment';
 
 function App() {
 
@@ -10,6 +12,8 @@ function App() {
    <Switch>
      <Route exact path="/" component={Ticket} />
      <Route path="/add-ticket" component={Addticket} />
+     <Route path="/view-comment/:id" component={Viewcomment} />
+     <Route path="/edit-ticket/:id" component={Editticket} />
    </Switch>
  </Router>
     );

8ed484f5251fdad3785076059288caa6d7db7b7b
NikolajManssonEsSolutions <nikolaj.mansson@essolutions.se>
2022-01-10 11:05:32 +0100

    Fetching get data into index page

--- src/pages/Ticket.js
+++ src/pages/Ticket.js
@@ -1,9 +1,54 @@
+import { tab } from '@testing-library/user-event/dist/tab';
+import axios from 'axios';
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import Loading from '../components/loading';
 
 class Ticket extends Component
 {
+
+    state = {
+        tickets: [],
+        loading: true,
+    }
+
+    async componentDidMount(){
+        const res = await axios.get('http://104.248.24.248/api/tickets');
+        console.log(res);
+        if(res.status === 200){
+            console.log('Set state');
+            
+            this.setState({
+                tickets: res.data.data,
+                loading:false
+            });
+            console.log('Set state on data');
+            console.log(res.data.data);
+
+        }
+    }
+
     render(){
+
+        var ticket_HTML_TABLE = "";
+        if(this.state.loading){
+            ticket_HTML_TABLE = <tr><td colSpan="6"><h2><Loading/></h2></td></tr>;
+        }else{
+            ticket_HTML_TABLE = 
+            this.state.tickets.map((item)=>{
+                return (
+                    <tr key={item.id}>
+                        <td>{item.id}</td>
+                        <td>{item.title}</td>
+                        <td>{item.author}</td>
+                        <td><Link to={`view-comment/${item.id}`} className="btn btn-link btn-sm">View comment</Link></td>
+                        <td><Link to={`edit-ticket/${item.id}`} className="btn btn-success btn-sm">Edit</Link></td>
+                        <td><button type="button" className="btn btn-danger btn-sm">Delete</button></td>
+                    </tr>
+                )
+            });
+        }
+
         return(
             <div className='container'>
                 <div className="row">
@@ -14,6 +59,21 @@ class Ticket extends Component
                                     <Link to={'add-ticket'} className="btn btn-primary btn-sm float-end">Add Ticket</Link>
                             </div>
                             <div className='card-body'>
+                                <table className="table table-bordered table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>Title</th>
+                                            <th>Author</th>
+                                            <th>View comment</th>
+                                            <th>Edit</th>
+                                            <th>Delete</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {ticket_HTML_TABLE}
+                                    </tbody>
+                                </table>
 
                             </div>
                         </div>
